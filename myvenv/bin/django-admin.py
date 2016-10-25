570ab316f38183b9913218aefcc92c99a887675d
Paola Katherine Cardoso Pacheco <paolakatherine@MacBook-Air-de-Paola.local>
2016-10-25 16:52:04 -0300

    settings change'

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/paolakatherine/Documents/Dev/tutorial-dg-110/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
